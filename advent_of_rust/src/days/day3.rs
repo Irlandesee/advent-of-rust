301350f720c83754930973bb4a556b0fde195847
Mattia Lunardi <mattiamauro.lunardi@elmec.it>
2025-09-01 21:50:19 +0200

    feat: solved day3 part 1

--- advent_of_rust/src/days/day3.rs
+++ advent_of_rust/src/days/day3.rs
@@ -9,13 +9,19 @@ pub fn solve_day3(input_file: PathBuf){
         .map(|line| line.to_string())
         .collect::<Vec<String>>();
 
-    let _ = contents.iter()
+    let total_res: i32 = contents.iter()
         .map(|line| {
-        println!("Line: {}", line);
-        let muls = extract_mul(line);
-        println!("{:?}", muls)
-    });
-
+            let muls = extract_mul(line);
+            let line_res: i32 = muls.iter()
+                .map(|token| {
+                    let (x, y) = extract_numbers(token);
+                    x * y
+                })
+                .sum();
+            line_res
+        })
+        .sum();
+    println!("Total res: {}", total_res);
 }
 
 fn extract_mul(line: &String) -> Vec<String> {
@@ -25,4 +31,24 @@ fn extract_mul(line: &String) -> Vec<String> {
     RE.find_iter(line)
         .map(|cap| cap.as_str().to_string())
         .collect()
+}
+
+fn extract_numbers(token: &String) -> (i32, i32) {
+    lazy_static! {
+        static ref RE: Regex = Regex::new("([0-9]+,[0-9]+)").unwrap();
+    }
+    let (x, y) = RE.find_iter(token)
+        .filter_map(|cap| {
+            let numbers: Vec<i32> = cap.as_str()
+                .split(",")
+                .filter_map(|num| num.trim().parse().ok())
+                .collect();
+            match numbers.len() {
+                2 => Some((numbers[0], numbers[1])),
+                _ => None
+            }
+        })
+        .next()
+        .expect("No valid number pair found");
+    (x, y)
 }
